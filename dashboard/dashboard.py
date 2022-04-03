57a2dfbc9586d95f74c66229a957f202870cdd92
AloneAlongLife <xd0931021@gmail.com>
2022-04-03 10:55:06 +0800

    Auto Commit

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -63,18 +63,7 @@ for i in range(5):
 
 class Web_UI():
     app = Flask(__name__)
-
-    @app.after_request
-    def add_header(r):
-        """
-        Add headers to both force latest IE rendering engine or Chrome Frame,
-        and also to cache the rendered page for 10 minutes.
-        """
-        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-        r.headers["Pragma"] = "no-cache"
-        r.headers["Expires"] = "0"
-        r.headers['Cache-Control'] = 'public, max-age=0'
-        return r
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
     @app.route('/', methods=['GET', 'POST'])
     def index():
